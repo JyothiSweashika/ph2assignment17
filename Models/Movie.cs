22d793eb0b80617f7228b76a4a613f4d0ba0216c
JyothiSweashika <jyothisweashikaraavi@gmail.com>
2023-09-04 15:34:28 +0530

    assignment17

---
+++ Models/Movie.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace assignment17.Models
+{
+    [Table("Movie")]
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public string Director { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public string Genre { get; set; }
+        public double Rating { get; set; }
+    }
+}
+
